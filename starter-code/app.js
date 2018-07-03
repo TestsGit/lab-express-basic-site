596139ce1da8d8ca82d059868aaf77678b3dde9a
Susana <susana_vadillo@yahoo.es>
2018-07-03 10:41:52 +0200

    probando

---
+++ starter-code/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (request, response, next) => {
+  console.log(request);
+  response.send('<h1>Welcome Ironhacker. :)</h1>');
+});
+
+app.listen(3000, () => {
+  console.log('My first app listening on port 3000!')
+});
